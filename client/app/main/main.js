095944151768ac5e4ad030567c4c96463a06711a
Christian <christ_sapphire@yahoo.com>
2014-08-15 23:04:05 -0400

    first commit

---
+++ client/app/main/main.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('mommyMealShareApp')
+  .config(function ($stateProvider) {
+    $stateProvider
+      .state('main', {
+        url: '/',
+        templateUrl: 'app/main/main.html',
+        controller: 'MainCtrl'
+      });
+  });
